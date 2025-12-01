3999899a699b2a4c04eb6944bfd2f2cc034bb9da
Felipe2620 <felipejurado032604@gamil.com>
2025-12-01 00:06:48 -0500

    Replace WhatsApp PNG with SVG and update contact number in Footer component GITPAGES-PUSH

--- Data/Services/WhatsAppService.js
+++ Data/Services/WhatsAppService.js
@@ -1,7 +1,7 @@
 // Data/Services/WhatsAppService.js
 
 export function enviarWhatsApp(producto, talla, color) {
-  const numero = "573002550081";
+  const numero = "573153750438";
 
   const mensaje = `Hola, estoy interesad@ en la prenda: ${producto.nombre}, Color: ${color}, Talla: ${talla}.`;
 
